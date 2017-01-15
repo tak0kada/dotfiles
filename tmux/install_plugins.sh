8c61605e6c64178f2975c7a3a515b72333fb5ff1
tak0kada <okokada.takuya@gmail.com>
2017-01-15 23:54:03 +0900

    add battery display

--- tmux/install_plugins.sh
+++ tmux/install_plugins.sh
@@ -3,3 +3,6 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # symlink
 ln ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+
+# battery displaying script
+chmod 755 ~/dotfiles/tmux/battery
